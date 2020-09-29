43367f456d24b2eca21af080fad773eed29b5de6
carlhaeggman <carl.haeggman02@gmail.com>
2020-09-29 09:54:44 +0200

    Flyttade på saker i scene

--- Ninja Run 3D Extreme/Assets/Scripts/Hazzards/HazzardDisappearingBlock.cs
+++ Ninja Run 3D Extreme/Assets/Scripts/Hazzards/HazzardDisappearingBlock.cs
@@ -51,7 +51,7 @@ public class HazzardDisappearingBlock : MonoBehaviour
         } 
         if(changeColor == true)
         {
-            EmissionColor();
+         
             ChangeColor();
         }
     }

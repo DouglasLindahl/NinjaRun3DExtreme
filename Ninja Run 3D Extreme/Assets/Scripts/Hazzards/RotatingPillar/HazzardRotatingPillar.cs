39fc6cd2a5e71fd85f7e690dc332781e668b5068
carlhaeggman <carl.haeggman02@gmail.com>
2020-11-09 10:05:10 +0100

    snörrsak

---
+++ Ninja Run 3D Extreme/Assets/Scripts/Hazzards/RotatingPillar/HazzardRotatingPillar.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazzardRotatingPillar : MonoBehaviour
+{
+    void Update()
+    {
+
+        transform.Rotate(new Vector3(0f, 0f, 200f) * Time.deltaTime);
+    }
+}

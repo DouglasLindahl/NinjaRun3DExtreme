106aa09d5a4fc2af57b12a95f034cc67d0622735
DouglasLindahl <71632655+DouglasLindahl@users.noreply.github.com>
2020-11-30 10:58:14 +0100

    Fixar death meny

---
+++ Ninja Run 3D Extreme/Assets/Scripts/Miscellaneous/PlayerDead.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerDead : MonoBehaviour
+{
+    public GameObject deathMenu;
+    public float deathTimer;
+
+    void restartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("Ree");
+            if(deathTimer <= 0)
+            {
+                deathMenu.SetActive(true);
+            }
+            else
+            {
+                deathTimer -= Time.deltaTime;
+            }
+        }
+    }
+}

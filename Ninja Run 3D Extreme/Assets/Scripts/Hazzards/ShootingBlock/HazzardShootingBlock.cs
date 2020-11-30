e2a0fa1bcda0de0e548429f870a064790b8505f6
carlhaeggman <carl.haeggman02@gmail.com>
2020-11-30 10:55:25 +0100

    Wow

---
+++ Ninja Run 3D Extreme/Assets/Scripts/Hazzards/ShootingBlock/HazzardShootingBlock.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazzardShootingBlock : MonoBehaviour
+{
+    public GameObject projectile;
+    GameObject player;
+    Transform playerPos;
+    public float t;
+    public float shootForce;
+    bool canShoot;
+    public float maxDistance;
+    private static float distanceAB;
+    public Transform projectileOrigin;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        canShoot = true;
+        Debug.Log(player);
+    }
+    private void FixedUpdate()
+    {
+        distanceAB = Vector3.Distance(transform.position, playerPos.position);
+    }
+
+    private void Update()
+    {
+        playerPos = player.transform;
+        //distanceBA = Vector3.Distance(playerPos.position, transform.position);
+        Debug.Log(distanceAB);
+        //Debug.Log(distanceBA);
+        if (canShoot == true && maxDistance > distanceAB /*|| maxDistance > distanceBA*/)
+        {
+            StartCoroutine(ShootProjectile());
+        }
+
+        DrawRaycast();
+    }
+    IEnumerator ShootProjectile()
+    {
+        StopCoroutine(ShootProjectile());
+        GameObject shot = GameObject.Instantiate(projectile, projectileOrigin.position, this.gameObject.transform.rotation);
+        shot.GetComponent<Rigidbody>().AddForce(transform.forward * shootForce);
+        canShoot = false;
+        yield return new WaitForSeconds(t);
+        canShoot = true;
+    }
+
+    private void DrawRaycast()
+    {
+        Debug.DrawLine(transform.position, player.transform.position, Color.red);
+        Debug.DrawLine(transform.position, player.transform.position, Color.green, maxDistance);
+
+    }
+}

5d010de5ea0f39999916ff99581ba34aed3daf31
carlhaeggman <carl.haeggman02@gmail.com>
2020-12-07 11:55:13 +0100

    Script

--- Ninja Run 3D Extreme/Assets/Scripts/Hazzards/ShootingBlock/HazzardShootingBlock.cs
+++ Ninja Run 3D Extreme/Assets/Scripts/Hazzards/ShootingBlock/HazzardShootingBlock.cs
@@ -39,7 +39,7 @@ public class HazzardShootingBlock : MonoBehaviour
     IEnumerator ShootProjectile()
     {
         StopCoroutine(ShootProjectile());
-        GameObject shot = GameObject.Instantiate(projectile, projectileOrigin.position, this.gameObject.transform.rotation);
+        GameObject shot = GameObject.Instantiate(projectile, projectileOrigin.position, this.transform.rotation);
         canShoot = false;
         yield return new WaitForSeconds(t);
         canShoot = true;

00977be1d545f1844f90d29febf26372592644ca
xRap1d <71003166+xRap1d@users.noreply.github.com>
2021-01-25 11:07:29 +0100

    Ny sound manager prefab

--- Ninja Run 3D Extreme/Assets/Scripts/Miscellaneous/NextLevel.cs
+++ Ninja Run 3D Extreme/Assets/Scripts/Miscellaneous/NextLevel.cs
@@ -5,8 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
+    AudioSource victorySound;
+
+    private void Awake()
+    {
+        victorySound = GameObject.Find("VictorySoundFX").GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        victorySound.Play();
     }
 }

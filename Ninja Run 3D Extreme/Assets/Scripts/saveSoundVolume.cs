b2a915954f2c1c7aa26ac3e8387535bf4cb0ff2e
xRap1d <71003166+xRap1d@users.noreply.github.com>
2021-02-01 11:40:13 +0100

    SoundManager och grejer

---
+++ Ninja Run 3D Extreme/Assets/Scripts/saveSoundVolume.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class saveSoundVolume : MonoBehaviour
+{
+    static float savedMusicVolume;
+    static float savedSoundFXVolume;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

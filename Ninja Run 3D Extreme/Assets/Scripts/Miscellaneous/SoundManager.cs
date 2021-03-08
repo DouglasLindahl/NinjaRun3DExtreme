aca565489e7df0231e47c9af4921e24b544aab11
carlhaeggman <carl.haeggman02@gmail.com>
2021-03-08 11:41:02 +0100

    DOUGLAS!

--- Ninja Run 3D Extreme/Assets/Scripts/Miscellaneous/SoundManager.cs
+++ Ninja Run 3D Extreme/Assets/Scripts/Miscellaneous/SoundManager.cs
@@ -2,17 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class SoundManager : MonoBehaviour
 {
     float musicVolume;
-    int musicVolumeTextValue;
     public Scrollbar musicVolumeSlider;
     public Text musicVolumeText;
 
-    float soundEffectVolume;
-    int soundEffectVolumeTextValue;
     public Scrollbar soundEffectVolumeSlider;
     public Text soundEffectVolumeText;
 
@@ -21,9 +17,6 @@ public class SoundManager : MonoBehaviour
     public AudioSource dashSound;
     public AudioSource deathSound;
     public AudioSource victorySound;
-
-    GameObject[] mngr;
-    private Scene scene;
 
     private void Awake()
     {
@@ -35,39 +28,17 @@ public class SoundManager : MonoBehaviour
     void Start()
     {
         musicVolumeSlider.value = musicVolume;
-        soundEffectVolumeSlider.value =  soundEffectVolume;
     }
 
     // Update is called once per frame
     void Update()
     {
-        MusicVolume();
-        SoundEffectVolume();
         backgroundTrack.volume = musicVolume;
+        /*
         jumpSound.volume = soundEffectVolume;
         dashSound.volume = soundEffectVolume;
         deathSound.volume = soundEffectVolume;
-        victorySound.volume = soundEffectVolume;
-    }
-    void MusicVolume()
-    {
-        musicVolume = musicVolumeSlider.value;
-        musicVolumeTextValue = Mathf.RoundToInt(musicVolume * 100);
-        musicVolumeText.text = musicVolumeTextValue.ToString() + "%";
-    }
-    void SoundEffectVolume()
-    {
-        soundEffectVolume = soundEffectVolumeSlider.value;
-        soundEffectVolumeTextValue = Mathf.RoundToInt(soundEffectVolume * 100);
-        soundEffectVolumeText.text = soundEffectVolumeTextValue.ToString() + "%";
-    }
-
-    void OnSceneWasSwitched()
-    {
-        musicVolumeSlider = GameObject.Find("MusicVolumeSlider").GetComponent<Scrollbar>();
-        soundEffectVolumeSlider = GameObject.Find("SoundEffectVolumeSlider").GetComponent<Scrollbar>();
-        musicVolumeText = GameObject.Find("MusicVolumeText").GetComponent<Text>();
-        soundEffectVolumeText = GameObject.Find("SoundEffectVolumeText").GetComponent<Text>();
+        victorySound.volume = soundEffectVolume;*/
     }
    
 }

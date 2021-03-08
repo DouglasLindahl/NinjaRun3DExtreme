75c1cb24404e26c9b1e23ad1985fbb1f818b9994
DouglasLindahl <71632655+DouglasLindahl@users.noreply.github.com>
2021-03-08 12:27:09 +0100

    Fixade ljudet

--- Ninja Run 3D Extreme/Assets/Scripts/Miscellaneous/SoundManager.cs
+++ Ninja Run 3D Extreme/Assets/Scripts/Miscellaneous/SoundManager.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class SoundManager : MonoBehaviour
 {
     float musicVolume;
+    float soundEffectVolume;
+
     public Scrollbar musicVolumeSlider;
     public Text musicVolumeText;
 
@@ -24,21 +26,31 @@ public class SoundManager : MonoBehaviour
         soundEffectVolumeSlider = GameObject.Find("SoundEffectVolumeSlider").GetComponent<Scrollbar>();
         musicVolumeText = GameObject.Find("MusicVolumeText").GetComponent<Text>();
         soundEffectVolumeText = GameObject.Find("SoundEffectVolumeText").GetComponent<Text>();
+        musicVolume = PlayerPrefs.GetFloat("MusicVolume");
+        soundEffectVolume = PlayerPrefs.GetFloat("SoundEffectVolume");
     }
     void Start()
-    {
+    {        
         musicVolumeSlider.value = musicVolume;
+        soundEffectVolumeSlider.value = soundEffectVolume;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        musicVolume = musicVolumeSlider.value;
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+        musicVolumeText.text = (musicVolume * 100).ToString("f0") + "%";
+        soundEffectVolume = soundEffectVolumeSlider.value;
+        PlayerPrefs.SetFloat("SoundEffectVolume", soundEffectVolume);
+        soundEffectVolumeText.text = (soundEffectVolume * 100).ToString("f0") + "%";
+
         backgroundTrack.volume = musicVolume;
-        /*
+        
         jumpSound.volume = soundEffectVolume;
         dashSound.volume = soundEffectVolume;
         deathSound.volume = soundEffectVolume;
-        victorySound.volume = soundEffectVolume;*/
-    }
-   
+        victorySound.volume = soundEffectVolume;        
+    }   
 }

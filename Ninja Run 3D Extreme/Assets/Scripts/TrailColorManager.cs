de0d58411506cbe2a39e35049b91eac58334c6a7
carlhaeggman <carl.haeggman02@gmail.com>
2021-03-22 12:08:35 +0100

    koolllttt

--- Ninja Run 3D Extreme/Assets/Scripts/TrailColorManager.cs
+++ Ninja Run 3D Extreme/Assets/Scripts/TrailColorManager.cs
@@ -9,17 +9,6 @@ public class TrailColorManager : MonoBehaviour
 
     public Color currentTrailColor;
 
-    private void Start()
-    {
-        colors[0] = Color.black;
-        colors[1] = new Color();
-        colors[2] = Color.green;
-        colors[3] = Color.blue;
-        colors[4] = Color.cyan;
-        colors[5] = new Color();
-        colors[6] = Color.gray; 
-    }
-
     public void ChangeColor(Color selectedColor)
     {
 

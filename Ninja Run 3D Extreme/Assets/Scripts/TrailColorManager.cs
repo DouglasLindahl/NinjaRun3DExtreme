0ec94bb19e496016e445a549c8c1a53129648100
xRap1d <carl.backman@elev.ga.lbs.se>
2021-03-22 11:31:23 +0100

    Test oasiuodbasd

---
+++ Ninja Run 3D Extreme/Assets/Scripts/TrailColorManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailColorManager : MonoBehaviour
+{
+    static public Color black, purple, red, green, blue, cyan, pink, silver;
+
+    static public Color currentTrailColor;
+
+    private void Start()
+    {
+        black = Color.black;
+        purple = new Color();
+        green = Color.green;
+        blue = Color.blue;
+        cyan = Color.cyan;
+        pink = new Color();
+        silver = Color.gray; 
+    }
+
+    void start()
+    {
+        currentTrailColor = black;
+    }
+}
